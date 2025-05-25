8100d70f35ee5577f93ab725b5d800abf6536d82
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-05-25 16:25:49 +0000

    Implement lcm function.

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -10,6 +10,10 @@ fn gcd(mut a: i64, mut b: i64) -> i64 {
     a.max(b)
 }
 
+fn lcm(a: i64, b: i64) -> i64 {
+    (a / gcd(a, b)) * b
+}
+
 fn main() {
     let a = 270;
     let b = 192;
@@ -18,4 +22,12 @@ fn main() {
     let a = 192;
     let b = 270;
     println!("gcd({a}, {b}) = {}", gcd(a, b));
+
+    let a = 12;
+    let b = 18;
+    println!("gcd({a}, {b}) = {}", gcd(a, b));
+
+    let a = 12;
+    let b = 18;
+    println!("lcm({a}, {b}) = {}", lcm(a, b));
 }
