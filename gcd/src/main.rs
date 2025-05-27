6543eb173a0709d899270387984e12225c855909
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-05-27 23:16:48 +0000

    Loop for user input.

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -43,8 +43,10 @@ fn main() {
     println!("gcd({a}, {b}) = {}", gcd(a, b));
     println!("lcm({a}, {b}) = {}", lcm(a, b));
 
-    let a = get_i64("a = ");
-    let b = get_i64("b = ");
-    println!("gcd({a}, {b}) = {}", gcd(a, b));
-    println!("lcm({a}, {b}) = {}", lcm(a, b));
+    loop {
+        let a = get_i64("a = ");
+        let b = get_i64("b = ");
+        println!("gcd({a}, {b}) = {}", gcd(a, b));
+        println!("lcm({a}, {b}) = {}", lcm(a, b));
+    }
 }
