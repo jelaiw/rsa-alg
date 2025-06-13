f35f02602bc384840d98fae400806bd9aaea3801
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-06-13 02:55:48 +0000

    Loop for user input.

--- src/bin/exp.rs
+++ src/bin/exp.rs
@@ -1,4 +1,4 @@
-use rsa_alg::fast_exp;
+use rsa_alg::{fast_exp, get_i64};
 
 fn main() {
     let num = 8;
@@ -8,4 +8,10 @@ fn main() {
     let num = 7;
     let pow = 10;
     println!("{num} ^ {pow} = {}", fast_exp(num, pow));
+
+    loop {
+        let num = get_i64("num = ");
+        let pow = get_i64("pow = ");
+        println!("{num} ^ {pow} = {}", fast_exp(num, pow));
+    }
 }
