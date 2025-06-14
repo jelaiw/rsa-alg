332bdff45da1f7ff4a3572bf2c2e39ed05d4c1f4
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-06-14 02:10:15 +0000

    Validate results with provided test data.

--- src/bin/exp.rs
+++ src/bin/exp.rs
@@ -13,6 +13,18 @@ fn main() {
     println!("{num} ^ {pow} = {}", fast_exp(num, pow));
     println!("{num} ^ {pow} % {modulus} = {}", fast_exp_mod(num, pow, modulus));
 
+    let num = 9;
+    let pow = 13;
+    let modulus = 283;
+    println!("{num} ^ {pow} = {}", fast_exp(num, pow));
+    println!("{num} ^ {pow} % {modulus} = {}", fast_exp_mod(num, pow, modulus));
+
+    let num = 213;
+    let pow = 5;
+    let modulus = 1000;
+    println!("{num} ^ {pow} = {}", fast_exp(num, pow));
+    println!("{num} ^ {pow} % {modulus} = {}", fast_exp_mod(num, pow, modulus));
+
     loop {
         let num = get_i64("num = ");
         let pow = get_i64("pow = ");
