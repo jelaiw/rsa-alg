687a0296a931a3a8bec60348bd960413bdd1670b
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-07-15 23:34:39 +0000

    Show one example, the rest are unit tests now.

--- src/bin/fast_exp.rs
+++ src/bin/fast_exp.rs
@@ -7,24 +7,6 @@ fn main() {
     println!("{num} ^ {pow} = {}", fast_exp(num, pow));
     println!("{num} ^ {pow} % {modulus} = {}", fast_exp_mod(num, pow, modulus));
 
-    let num = 7;
-    let pow = 10;
-    let modulus = 101;
-    println!("{num} ^ {pow} = {}", fast_exp(num, pow));
-    println!("{num} ^ {pow} % {modulus} = {}", fast_exp_mod(num, pow, modulus));
-
-    let num = 9;
-    let pow = 13;
-    let modulus = 283;
-    println!("{num} ^ {pow} = {}", fast_exp(num, pow));
-    println!("{num} ^ {pow} % {modulus} = {}", fast_exp_mod(num, pow, modulus));
-
-    let num = 213;
-    let pow = 5;
-    let modulus = 1000;
-    println!("{num} ^ {pow} = {}", fast_exp(num, pow));
-    println!("{num} ^ {pow} % {modulus} = {}", fast_exp_mod(num, pow, modulus));
-
     loop {
         let num = get_i64("num = ");
         let pow = get_i64("pow = ");
