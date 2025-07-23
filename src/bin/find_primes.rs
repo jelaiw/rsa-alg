adef45ec9afe86f86d7d245a6c3ba2b05c9db38c
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-07-23 02:16:20 +0000

    Don't be blindly stupid. :-D

--- src/bin/find_primes.rs
+++ src/bin/find_primes.rs
@@ -4,7 +4,7 @@ use rsa_alg::is_probably_prime;
 fn find_prime(min: i64, max: i64, num_tests: u8) -> i64 {
     loop {
         let x = rand::rng().random_range(min..=max);
-        if is_probably_prime(x, num_tests) {
+        if x % 2 != 0 && is_probably_prime(x, num_tests) {
             return x;
         }
     }
